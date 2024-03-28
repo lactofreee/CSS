7d7d44180367fff9af83457a17f0b83fff488a1c
lactofreee <lactofreee@gmail.com>
2024-03-29 04:18:12 +0900

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -19,43 +19,43 @@ function App() {
         <div className="cards__card">
           <h2 className="card__title">The Making of Modern...</h2>
           <div className="card__footer">
-            <div className="price">price</div>
-            <div className="length">length</div>
+            <div className="price">Free</div>
+            <div className="length">2</div>
           </div>
         </div>
         <div className="cards__card">
           <h2 className="card__title">Movile Interfaces Advanced</h2>
           <div className="card__footer">
-            <div className="price">price</div>
-            <div className="length">length</div>
+            <div className="price">Free</div>
+            <div className="length">2</div>
           </div>
         </div>
         <div className="cards__card">
           <h2 className="card__title">Graphic Design. Professium</h2>
           <div className="card__footer">
-            <div className="price">price</div>
-            <div className="length">length</div>
+            <div className="price">$199 / month</div>
+            <div className="length">9 months</div>
           </div>
         </div>
         <div className="cards__card">
           <h2 className="card__title">Web Design Junior</h2>
           <div className="card__footer">
-            <div className="price">price</div>
-            <div className="length">length</div>
+            <div className="price">Free</div>
+            <div className="length">3</div>
           </div>
         </div>
         <div className="cards__card">
           <h2 className="card__title">Inclusive Web Design</h2>
           <div className="card__footer">
-            <div className="price">price</div>
-            <div className="length">length</div>
+            <div className="price">$99 / month</div>
+            <div className="length">3</div>
           </div>
         </div>
         <div className="cards__card">
           <h2 className="card__title">Machine Learning</h2>
           <div className="card__footer">
-            <div className="price">price</div>
-            <div className="length">length</div>
+            <div className="price">$59 / month</div>
+            <div className="length">3 months</div>
           </div>
         </div>
       </div>
